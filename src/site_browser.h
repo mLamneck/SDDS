3022a8b22c94d02eee4698cbc59425a9e71cc88a
Mark Lamneck <mark.lamneck@mpic.de>
2024-02-25 12:26:21 +0100

    moving esp based stuff to a separate repo in order to avoid compile errors on none esp platforms

--- src/site_browser.h
+++
@@ -1,3 +0,0 @@
-const byte site_browser[] PROGMEM = {
-  #include "site_browserHtml.hex.h"
-};
--- test/testStringsU.h
+++ test/testStringsU.h
@@ -1,9 +1,52 @@
 #include "uStrings.h"
 #include "uTime.h"
 
+typedef std::function<bool()> TtestProc;
+typedef bool (*TtestProcSimple)(Tdescr*);
+
 class TtestCase{
+    private:
+        const char* FtestUnit;
+        int Fsucc = 0;
+        int Ffailed = 0;
+        char FlogBuffer[1024];
+
+    protected:
+        char* binToHex(void* _buf, int size){
+            uint8* buf = reinterpret_cast<uint8*>(_buf); 
+            int run = 0;
+            for (auto i = 0; i<size; i++){
+                run += sprintf(&FlogBuffer[run],"0x%.2X ",buf[i]);
+            }
+            return &FlogBuffer[0];
+        }
+
+        void doTest(TtestProc _tp, const char* _name){
+            debug::log("--------------- running test %s",_name);
+            try{
+                if (_tp()) Fsucc++;
+                else Ffailed++;
+            }
+            //Why the heck is this asshole not catching exceptions here!?!?!?!?
+            //Fuck C++!!!!!!!!!!!! probably I have to set some flags in the build options
+            catch (...){
+                debug::log("exception");
+                Ffailed++;
+            }
+        }
+        virtual bool test() = 0;
     public:
-        virtual void test() = 0;
+        TtestCase(const char* _testUnit){
+            FtestUnit = _testUnit;
+        }
+
+        void runTests(){
+            debug::log("XXXXXXXXXXXXXXXXXXXX running tests for %s XXXXXXXXXXXXXXXXXXXXXXXXXX",FtestUnit);
+            test();
+            debug::log("XXXXXXXXXXXXXXXXXXXX test results for %s XXXXXXXXXXXXXXXXXXXXXXXXXX",FtestUnit);
+            debug::log("nTests = %d test failed = %d",Fsucc+Ffailed,Ffailed);
+            debug::log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+        }
 };
 
 void testStrings(){
@@ -61,5 +104,12 @@ void testTimeConv(){
     int i = 0;
     if (i > 0){
 
+    }
+}
+
+namespace testStringsU{
+    void test(){
+        testStrings();
+        testTimeConv();
     }
 }
