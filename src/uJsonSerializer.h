c03f8de7061e7edcde0c219f422a5fc6c34f9221
Mark Lamneck <mark.lamneck@mpic.de>
2024-03-09 10:07:03 +0100

    - serialize value array - string encoding - added static methods

--- src/uJsonSerializer.h
+++ src/uJsonSerializer.h
@@ -18,7 +18,55 @@ class TjsonSerializer{
       FwithValue = _withValue;
     }
 
-    void serialize(Tdescr* _d){
+    static void encodeString(Tstream* _stream, const char* _str){
+      while (*_str != '\0'){
+        switch(*_str){
+          case '"': case '\\': case '/':
+          case '\b': case '\f': case '\n':
+          case '\r': case '\t':
+            _stream->write('\\');
+            break;
+        }
+        _stream->write(*_str++);
+      }
+    }
+
+    static void serializeAsString(Tstream* _stream, const char* _str){
+      _stream->write('"');
+      TjsonSerializer::encodeString(_stream,_str);
+      _stream->write('"');
+    }
+
+    static void serializeAsString(Tstream* _stream, Tdescr* _d){
+      TjsonSerializer::serializeAsString(_stream,_d->to_string().c_str());
+    }
+
+    void serializeAsString(Tdescr* _d){
+      TjsonSerializer::serializeAsString(Fstream,_d);
+    }
+
+    static void serializeValues(Tstream* _stream, TmenuHandle* _struct, TrangeItem _first, TrangeItem _last){
+        _stream->write("[");
+        auto it = _struct->iterator(_first);
+        if (it.hasNext()){
+          do{
+            Tdescr* d = it.next();
+            switch(d->type()){
+              case(sdds::Ttype::STRING): case(sdds::Ttype::TIME):
+                TjsonSerializer::serializeAsString(_stream,d);
+                break;
+              default:
+                _stream->write(d->to_string().c_str());
+                break;
+            }
+            if (!it.hasNext() || (_first++ >= _last)) break;
+            _stream->write(',');
+          } while(true);
+        }
+        _stream->write("]");
+    }
+
+    void serializeTypeDescr(Tdescr* _d){
       Fstream->write(json_key("type"));
       Fstream->write(_d->typeId());
       Fstream->write(',');
@@ -29,9 +77,21 @@ class TjsonSerializer{
       Fstream->write('"');
       Fstream->write(_d->name());
       Fstream->write('"');
-      if (!FwithValue && !_d->isStruct()) return;
-      Fstream->write(',');
-      Fstream->write(json_key("value"));
+
+      //for enums, write list of enums
+      if (_d->type() == sdds::Ttype::ENUM){
+          Fstream->write(',');
+          Fstream->write(json_key("enums"));
+          Fstream->write('[');
+          TenumBase* en = static_cast<TenumBase*>(_d);
+          auto enumCnt = en->enumCnt();
+          for (auto i=0; i<enumCnt; i++){
+            TjsonSerializer::serializeAsString(Fstream,en->getEnum(i));
+            if (i+1 >= enumCnt) break;
+            Fstream->write(',');
+          }
+          Fstream->write(']');
+      }
     }
 
     void serialize(TmenuHandle* _curr){
@@ -41,41 +101,28 @@ class TjsonSerializer{
         Tdescr* d = it.next();
 
         Fstream->write('{');
-        serialize(d);
+        serializeTypeDescr(d);
         if (d->isStruct()){
+          Fstream->write(',');
+          Fstream->write(json_key("value"));
           TmenuHandle* mh = static_cast<Tstruct*>(d)->value();
           if (mh) serialize(mh);
           else Fstream->write("null");
         }
+
         else if (FwithValue){
-          if (d->isEnum()){
-            //provide as value???
-            Fstream->write('"');
-            Fstream->write(d->to_string().c_str());
-            Fstream->write('"');
-            Fstream->write(',');
-            Fstream->write(json_key("enums"));
-            Fstream->write('[');
-            TenumBase* en = static_cast<TenumBase*>(d);
-            auto enumCnt = en->enumCnt();
-            for (auto i=0; i<enumCnt; i++){
-                Fstream->write('"');
-                Fstream->write(en->getEnum(i));
-                Fstream->write('"');
-                if (i+1 >= enumCnt) break;
-                Fstream->write(',');
-            }
-            Fstream->write(']');
-          }
-          else{
-            bool quotes = d->needQuotes();
-            if (quotes) Fstream->write('"');
-            Fstream->write(d->to_string().c_str()); //toDo... strings?
-            if (quotes) Fstream->write('"');
+          Fstream->write(',');
+          Fstream->write(json_key("value"));
+
+          switch(d->type()){
+            case sdds::Ttype::ENUM: case sdds::Ttype::STRING: case sdds::Ttype::TIME:
+              serializeAsString(d);
+              break;
+            default:
+              Fstream->write(d->to_string().c_str());
           }
         }
         Fstream->write('}');
-
         if (it.hasNext()) Fstream->write(',');
       }
       Fstream->write(']');
