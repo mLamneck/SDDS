521156d4779baa9b9a45c2c86d920862364cdaf3
Mark Lamneck <mark.lamneck@mpic.de>
2024-04-05 10:32:32 +0200

    example for platformIO added

---
+++ examples/platformIO/led/src/main.cpp
@@ -0,0 +1,62 @@
+#include "uTypedef.h"
+#include "uMultask.h"
+#include "uParamSave.h"
+
+sdds_enum(OFF,ON) TonOffState;
+
+class Tled : public TmenuHandle{
+    Ttimer timer;
+    public:
+      sdds_struct(
+          sdds_var(TonOffState,ledSwitch,sdds::opt::saveval)
+          sdds_var(TonOffState,blinkSwitch,sdds::opt::saveval)
+          sdds_var(Tuint16,onTime,sdds::opt::saveval,500)
+          sdds_var(Tuint16,offTime,sdds::opt::saveval,500)
+      )
+      Tled(){
+          pinMode(LED_BUILTIN, OUTPUT);
+
+          on(ledSwitch){
+              if (ledSwitch == TonOffState::dtype::ON) digitalWrite(LED_BUILTIN,0);
+              else digitalWrite(LED_BUILTIN,1);
+          };
+
+          on(blinkSwitch){
+              if (blinkSwitch == TonOffState::dtype::ON) timer.start(0);
+              else timer.stop();
+          };
+
+          on(timer){
+              if (ledSwitch == TonOffState::dtype::ON){
+                  ledSwitch = TonOffState::dtype::OFF;
+                  timer.start(offTime);
+              } 
+              else {
+                  ledSwitch = TonOffState::dtype::ON;
+                  timer.start(onTime);
+              }
+          };
+      }
+};
+
+class TuserStruct : public TmenuHandle{
+  public:
+    sdds_struct(
+        sdds_var(Tled,led)
+        sdds_var(TparamSaveMenu,params)
+    )
+    TuserStruct(){
+        //you application code goes here... 
+    }
+} userStruct;
+
+#include "uSerialSpike.h"
+TserialSpike serialHandler(userStruct,115200);
+
+void setup(){
+
+}
+
+void loop(){
+  TtaskHandler::handleEvents();
+}
